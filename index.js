2edd8bd1724cb4b81e2de7bdf66ee23f1ef84bd9
eshikaalam <eshikaalamsharia@gmail.com>
2025-05-21 21:50:53 +0600

    final

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 // Connect to local MySQL
 const db = mysql.createConnection({
   host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
   user:     process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
